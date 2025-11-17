c39bb720484672507ed0916f48ab0667d3ea2481
Ayabn2004 <sunfloweer21@gmail.com>
2025-11-17 19:21:29 +0100

    fileImport.test.js

--- Lab4_Jest/fileImport.test.js
+++ Lab4_Jest/fileImport.test.js
@@ -2,9 +2,9 @@ const { s1, s2, m1, m2 } = require('./fileImport');
 const { mean } = require('./notation');
 
 test('m1 is correct mean of s1', () => {
-  expect(m1).toBe(mean(s1)); // should be 13
+  expect(m1).toBe(mean(s1)); 
 });
 
 test('m2 is correct mean of s2', () => {
-  expect(m2).toBe(mean(s2)); // should be 19
+  expect(m2).toBe(mean(s2)); /
 });
